79dbaa909f2081154be79089fe55d4ba28232132
Stephen Telesmanic <steve.telesmanic@gmail.com>
2018-04-22 08:26:14 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,6 +2,7 @@ def roll_call_dwarves(array)# code an argument here
   # Your code here
   array.each.with_index do |name, i|
     puts "#{i} #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
